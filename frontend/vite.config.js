5ee24e33a0e86223df5468c1406ba7b2f217e981
purejoymeditations <spiritualleanne@gmail.com>
2025-11-07 13:17:00 +0530

    vite config to frontend/dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../api/frontend-dist", // build output goes inside backend
+    outDir: "../frontend/dist", // build output goes inside backend
   },
   server: {
     proxy: {
